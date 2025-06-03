94212abb9e6ce98502a45756da77f42d416c8a5f
k-elven <2279356591@qq.com>
2025-06-03 10:19:13 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,26 @@
 import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
 import AuthRoute from '@/components/AuthRoute'
-import Home from "@/pages/Home";
-import Publish from "@/pages/Publish";
-import Article from "@/pages/Article";
+// import Home from "@/pages/Home";
+// import Publish from "@/pages/Publish";
+// import Article from "@/pages/Article";
 
 import { createBrowserRouter } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
+//路由懒加载
+const Home = lazy(() => import('@/pages/Home'))
+const Article = lazy(() => import('@/pages/Article'))
+const Publish = lazy(() => import('@/pages/Publish'))
 
 const router = createBrowserRouter([
     {
         path:'/',
         element:<AuthRoute><Layout /></AuthRoute>,
         children: [
-            { index: true,element:<Home /> },
-            { path:'publish',element:<Publish /> },
-            { path:'article',element:<Article /> }
+            { index: true,element:<Suspense fallback={'加载中'}><Home /></Suspense> },
+            { path:'publish',element:<Suspense fallback={'加载中'}><Publish /></Suspense> },
+            { path:'article',element:<Suspense fallback={'加载中'}><Article /></Suspense> }
         ]
     },
     {
